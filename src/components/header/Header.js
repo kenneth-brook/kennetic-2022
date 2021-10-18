b36132d35f9b100160703e65fa62d04cbaa22041
Kenneth Brook <kenneth.brook@hotmail.com>
2021-10-17 22:36:50 -0400

    adjusted header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import kclogo from '../../assets/kclogo.png';
 
 function Header() {
     return (
         <div className="headWrap">
+            <div className="logoWrap">
+            <img src={kclogo} />
+            </div>
+            <div className="headTextWrap">
             <h1>Welcome to Kennetic Concepts</h1>
             <h4>Official site for the 2022 relaunch</h4>
+            </div>
         </div>
     );
 }
