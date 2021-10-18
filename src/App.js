2fcdb298d977e85d07677d11111828e7dec03184
Kenneth Brook <kenneth.brook@hotmail.com>
2021-10-17 20:14:50 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import kclogo from './assets/kclogo.png';
 
-import Header from './components/Header';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
     <div className="logo">
-      
       <img src={kclogo} />
     </div>
+      <Footer />
     </div>
   );
 }
