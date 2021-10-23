d56f47c99707a501d67fd6eac31fd83e7beb0d7c
Kenneth Brook <kenneth.brook@hotmail.com>
2021-10-23 12:00:15 -0400

    started news card component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import kclogo from './assets/kclogo.png';
-
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
     <div>
       <Header />
-    <div className="logo">
-      <img src={kclogo} />
-    </div>
+      <div className="logo">
+        <Home />
+      </div>
       <Footer />
     </div>
   );
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import NewsBlock from "./newsBlock/NewsBlock";
+
 function Home() {
     return (
-        <div className="homeWrap"></div>
+        <div className="homeWrap">
+            <NewsBlock />
+            <NewsBlock />
+            <NewsBlock />
+            <NewsBlock />
+            <NewsBlock />
+            <NewsBlock />
+        </div>
     )
 }
+
+export default Home;
